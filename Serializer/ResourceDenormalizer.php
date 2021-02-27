ff988cbd9c81f3385e023ea30f98a302d740c6c1
g.uvarov <g.uvarov@bbq.agency>
2021-02-27 19:15:21 +0200

    Register ResourceNormalizer as a service. Add support of array denormalization

--- Serializer/ResourceDenormalizer.php
+++ Serializer/ResourceDenormalizer.php
@@ -2,8 +2,11 @@
 
 namespace Zim\Bundle\SymfonyRestHelperBundle\Serializer;
 
+use Doctrine\DBAL\Connection;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Zim\Bundle\SymfonyRestHelperBundle\Helper\StringHelper;
 
 class ResourceDenormalizer implements DenormalizerInterface
 {
@@ -22,7 +25,16 @@ class ResourceDenormalizer implements DenormalizerInterface
      */
     public function denormalize($data, string $type, string $format = null, array $context = [])
     {
-        return $this->em->find($type, $data);
+        if (is_scalar($data)) {
+            return $this->em->find($type, $data);
+        } else if (is_array($data)) {
+            $fqcn = str_replace('[]', '', $type);
+            $query = $this->em->createQuery('SELECT m FROM '.$fqcn.' m WHERE m.id IN(:ids)');
+            $query->setParameter('ids', $data);
+            return $query->getResult();
+        } else {
+            throw new \Exception('Can not denormalize value. Unsupported value type');
+        }
     }
 
     /**
@@ -30,20 +42,42 @@ class ResourceDenormalizer implements DenormalizerInterface
      */
     public function supportsDenormalization($data, string $type, string $format = null)
     {
-        if (false === is_scalar($data)) {
-            return false;
+        if (is_scalar($data)) {
+            if (false === class_exists($type)) {
+                return false;
+            }
+
+            $metadata = $this->em->getClassMetadata($type);
+
+            if (!$metadata) {
+                return false;
+            }
         }
 
-        if (!class_exists($type)) {
-            return false;
+        if (is_array($data)) {
+            if (count($data) === 0) {
+                return false;
+            }
+
+            if (false === StringHelper::endsWith($type, '[]')) {
+                return false;
+            }
+
+            // check array is real array (not associative)
+            $keys = array_keys($data);
+
+            if ($keys !== range(0, count($data) - 1)) {
+                return false;
+            }
+
+            $fqcn     = str_replace('[]', '', $type);
+            $metadata = $this->em->getClassMetadata($fqcn);
+
+            if (!$metadata) {
+                return false;
+            }
         }
 
-        $metadata = $this->em->getClassMetadata($type);
-
-        if (!$metadata) {
-            return false;
-        }
-
-        return true;
+        return is_scalar($data) || is_array($data);
     }
 }
